8f749e3ba34c1d73348e72f03238e2a618f9da89
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-19 22:25:16 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],
         !won? && full? ? true : false
       end
       def over?
-        won? || draw? ? true : false
+        draw? || won? ? true : false
         end
         def winner
             WIN_COMBINATIONS.find do |combo|
